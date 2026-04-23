e0c4914aeadabfd6d011388fc32d17c354429a8f
Javier E. <jaedns@gmail.com>
2026-04-23 13:37:13 -0300

    Add OrValidationErrors for Option and std::io::Result (#8)

--- rust/src/validator.rs
+++ rust/src/validator.rs
@@ -6,6 +6,18 @@ pub trait OrValidationErrors<T> {
     fn or_validation_errors(self) -> ValidationResult<T>;
 }
 
+impl<T> OrValidationErrors<T> for Option<T> {
+    fn or_validation_errors(self) -> ValidationResult<T> {
+        self.ok_or_else(Default::default)
+    }
+}
+
+impl<T> OrValidationErrors<T> for Result<T, std::io::Error> {
+    fn or_validation_errors(self) -> ValidationResult<T> {
+        self.map_err(|_| Default::default())
+    }
+}
+
 impl<T> OrValidationErrors<T> for Result<T, sqlx::Error> {
     fn or_validation_errors(self) -> ValidationResult<T> {
         self.map_err(|_| Default::default())
